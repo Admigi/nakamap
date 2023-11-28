bc9ceac758211c87ca6b0b28b1003df39853d340
Clément Duval <135060014+Admigi@users.noreply.github.com>
2023-11-28 12:25:46 +0100

    put a s to userbadge

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-  has_many :userbadge
+  has_many :userbadges
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
