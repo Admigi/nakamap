ee0d784335e92ee8568fe5936013aa2aa46e238f
Emmanuel LeBouedec <elebouedec@gmail.com>
2023-11-28 17:10:36 +0100

    pin controller and regex email devise

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,2 +1,17 @@
 class PinsController < ApplicationController
+
+  def index
+    @pins = Pin.all
+  end
+
+  def show
+    @pin = Pin.find(params[:id])
+    @post = Pin.post
+  end
+
+
+
+
+
+
 end
