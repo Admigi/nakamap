f799bdd7c6ba6b1b6fb46a832dd0893dca2487b3
Thomas Mace <mace.thomas49@gmail.com>
2023-12-04 12:30:44 +0100

    Quizzes handle + logic of the quizz

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,21 +1,39 @@
 class ChallengesController < ApplicationController
-  def show
-    @questions = [
-      { question: "What is the primary use of a katana?", options: ["Cutting", "Thrusting", "Throwing"], correct_answer_index: 0 },
-      { question: "Which Japanese swordsmith is famous for creating exceptional katanas?", options: ["Masamune", "Hattori Hanzo", "Miyamoto Musashi"], correct_answer_index: 0 },
-      { question: "What is the name of the curved guard at the end of the handle of a katana?", options: ["Tsuba", "Tsuka", "Saya"], correct_answer_index: 0 },
-    ]
-
-    question_index = params[:question].to_i
-    current_question = @questions[question_index]
-
-    @question = current_question[:question]
-    @options = current_question[:options]
-    @correct_answer_index = current_question[:correct_answer_index]
-
-    if params[:answer]
-      @user_answer_index = params[:answer][question_index].to_i
-      @correct = (@user_answer_index == @correct_answer_index)
-    end
+  def index
+    @challenges = Challenge.all
   end
+  
+  def show
+    @challenge = Challenge.find(params[:id])
+  end
+
+  def check_answer
+    @challenge = Challenge.find(params[:id])
+    questions = @challenge.metadata['questions']
+    total_questions = questions.length
+    correct_answers = 0
+  
+    selected_options = params[:challenge][:selected_options]
+  
+    unless selected_options.nil?
+      selected_options.each_with_index do |selected_option, index|
+        question = questions[index]
+  
+        # Debugging lines to check data and comparisons
+        puts "Question: #{question}"
+        puts "Correct Option (Challenge): #{@challenge.correct_option}"
+        puts "Correct Option (Question): #{question['correct_option']}"
+        puts "Selected Option: #{selected_option}"
+  
+        correct_option = question['correct_option']
+  
+        if selected_option == correct_option
+          correct_answers += 1
+        end
+      end
+    end
+  
+    flash[:notice] = "You answered #{correct_answers} out of #{total_questions} questions correctly."
+    redirect_to root_path
+  end  
 end
--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,2 +1,5 @@
 class Challenge < ApplicationRecord
+    serialize :metadata, JSON
+    store_accessor :metadata, :selected_options
+    has_many :options, dependent: :destroy
 end
