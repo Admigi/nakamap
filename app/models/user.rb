5ed5327e9076d47bbb58123db3158eca3a9c70ed
Thomas Mace <mace.thomas49@gmail.com>
2023-12-05 09:38:17 +0100

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   has_many :badges, through: :userbadges
   has_one_attached :image_url
 
-
   # validates :username, uniqueness: true, presence: true, length: { minimum: 3}
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
